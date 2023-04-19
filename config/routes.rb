e2b93f3d1807d2c212e36589438e9e17d91b5755
yetemegn-telaye <lemma19yet@gmail.com>
2023-04-19 18:16:41 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
-  resources :users do
-    resources :posts 
-  end
+  get "/users" , to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
 
 end
