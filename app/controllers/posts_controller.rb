da648538fa2c65b474ffb42652b3658db8d80a63
yetemegn-telaye <lemma19yet@gmail.com>
2023-04-22 15:07:09 +0300

    Add new action with the corresponding view and route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ActionController::Base
   before_action :user
   before_action :comments
 
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html {render :new, locals: {post: post}}
+    end
+  end
+
   def index
     @posts = @user.posts
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/users" , to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/new", to: "posts#new" 
   get "/users/:id/posts/:post_id", to: "posts#show"
 
 end
