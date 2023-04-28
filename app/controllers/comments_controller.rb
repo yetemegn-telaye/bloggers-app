aaa5b2a027b129f3eca3021ba775798e91008f4c
yetemegn-telaye <lemma19yet@gmail.com>
2023-04-29 01:48:40 +0300

    Fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  #load_and_authorize_resource
+  # load_and_authorize_resource
   def index
     @post = Post.find(params[:post_id])
     @comments = @post.comments
